6fc3f65afb6fc24977fa93b99d9b3f42e2fa1960
sammyukavi <sammyukavi@gmail.com>
2017-06-20 11:16:47 +0300

    Added some JQuery logic to make api calls

--- jquery-worldbank-data-catalog-browser/public_html/assets/js/app.js
+++ jquery-worldbank-data-catalog-browser/public_html/assets/js/app.js
@@ -1,3 +1,4 @@
+var resultsPerPage, resultsPerPage, pagesSelect, pageIndicator, datacatalogContainer, searchInput;
 $app = {
     postData: function (url, callback, data, dataType, requestType) {
         if (!data) {
@@ -60,7 +61,7 @@ $app = {
                 }
             },
             error: function (xhr) {
-                app.showNotification("Error fetching ajax", app.notifications.ERROR);
+                console.log(xhr.responseText)
             }, complete: function (xhr) {
 
             }
@@ -69,19 +70,19 @@ $app = {
 };
 
 $(document).ready(function () {
-    var resultsPerPage = $("#resultsPerPage");
-    var pagesSelect = $("#pages");
-    var pageIndicator = $("#pageIndicator");
-    var datacatalogContainer = $("#datacatalog");
+    resultsPerPage = $("#resultsPerPage");
+    pagesSelect = $("#pages");
+    pageIndicator = $("#pageIndicator");
+    datacatalogContainer = $("#datacatalog");
+    searchInput = $("#search");
 
     $("#loadCatalog").click(function () {
-        $app.getData("http://api.worldbank.org/v2/datacatalog?format=json&per_page=250", function (data) {
+        $app.getData("http://api.worldbank.org/v2/datacatalog/metatypes/name;description?format=json&per_page=" + resultsPerPage.val() + "&page=" + pagesSelect.val(), function (data) {
             //console.log(data);
-            
+            var str = '<ul id="catalogList">';
             //name,acronym,description,url,type,languagesupported,periodicity,economycoverage,granularity,numberofeconomies
             //topics,updatefrequency,updateschedule,lastrevisiondate,contactdetails,accessoption,bulkdownload,cite,detailpageurl
-           //popularity,coverage,api,apiaccessurl,apisourceid,mobileapp
-            
+            //popularity,coverage,api,apiaccessurl,apisourceid,mobileapp
             resultsPerPage.val(data.per_page)
             pagesSelect.find('option').remove().end().append('<option>Jump to page</option>');
             for (var i = 0, max = data.pages; i < max; i++) {
@@ -93,10 +94,94 @@ $(document).ready(function () {
             }
             pagesSelect.val(data.page);
             $(data.datacatalog).each(function (index, item) {
-                console.log(item.metatype);
+                var name, description;
+                name = description = "";
+                if (typeof item.metatype[0] !== "undefined") {
+                    name = item.metatype[0].value;
+                }
+                if (typeof item.metatype[1] !== "undefined") {
+                    description = item.metatype[1].value;
+                }
+                str += '<li attr-id="' + item.id + '"><div class="title">' + name + '</div><div>' + description + '</div>\n\
+<button>Filter using this catalog</button> <button>View</button> </li>';
             });
+            str += "</ul>";
+            datacatalogContainer.html(str);
+            initCallbacks();
         });
+    });
+
+
+    $("#searchCatalog").click(function () {
+        var search = searchInput.val();
+        if (search.length < 3) {
+            alert("Input stuff to search. Minimum is 3 characters");
+        } else {
+            $app.getData("http://api.worldbank.org/v2/datacatalog/search/" + search + "?format=json&per_page=" + resultsPerPage.val() + "&page=" + pagesSelect.val(), function (data) {
+                var str = '<ul id="catalogList">';
+                resultsPerPage.val(data.per_page)
+                pagesSelect.find('option').remove().end().append('<option>Jump to page</option>');
+                for (var i = 0, max = data.pages; i < max; i++) {
+                    pagesSelect.append($('<option>',
+                            {
+                                value: (i + 1),
+                                text: "Page " + (i + 1)
+                            }));
+                }
+                pagesSelect.val(data.page);
+                $(data.datacatalog).each(function (index, item) {
+                    var name, description;
+                    name = description = "";
+                    if (typeof item.metatype[0] !== "undefined") {
+                        name = item.metatype[0].value;
+                    }
+                    if (typeof item.metatype[2] !== "undefined") {
+                        description = item.metatype[2].value;
+                    }
+                    str += '<li attr-id="' + item.id + '"><div class="title">' + name + '</div><div>' + description + '</div></li>';
+                });
+                str += "</ul>";
+                datacatalogContainer.html(str);
+                initCallbacks();
+            });
+        }
     });
 });
 
+function initCallbacks() {
+    $(".title").click(function () {
+        var id = $(this).parent().attr("attr-id");
+        $app.getData("http://api.worldbank.org/v2/datacatalog/" + id + "/metatypes/name;description?format=json&per_page=" + resultsPerPage.val() + "&page=" + pagesSelect.val(), function (data) {
+            //console.log(data);
+            var str = '<ul id="catalogList">';
+            //name,acronym,description,url,type,languagesupported,periodicity,economycoverage,granularity,numberofeconomies
+            //topics,updatefrequency,updateschedule,lastrevisiondate,contactdetails,accessoption,bulkdownload,cite,detailpageurl
+            //popularity,coverage,api,apiaccessurl,apisourceid,mobileapp
+            resultsPerPage.val(data.per_page)
+            pagesSelect.find('option').remove().end().append('<option>Jump to page</option>');
+            for (var i = 0, max = data.pages; i < max; i++) {
+                pagesSelect.append($('<option>',
+                        {
+                            value: (i + 1),
+                            text: "Page " + (i + 1)
+                        }));
+            }
+            pagesSelect.val(data.page);
+            $(data.datacatalog).each(function (index, item) {
+                var name, description;
+                name = description = "";
+                if (typeof item.metatype[0] !== "undefined") {
+                    name = item.metatype[0].value;
+                }
+                if (typeof item.metatype[1] !== "undefined") {
+                    description = item.metatype[1].value;
+                }
+                str += '<li attr-id="' + item.id + '"><div class="title">' + name + '</div><div>' + description + '</div></li>';
+            });
+            str += "</ul>";
+            datacatalogContainer.html(str);
+
+        });
+    });
+}
 
